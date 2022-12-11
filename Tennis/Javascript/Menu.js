57cc11fc2dd5f16e3e3acbad734c7f94bc4f5ab0
MartKarl <Martin.Karlsson95@hotmail.com>
2022-12-11 22:01:57 +0100

    Fixade det sista inför inlämning

--- Tennis/Javascript/Menu.js
+++ Tennis/Javascript/Menu.js
@@ -1,11 +1,13 @@
 
-
+//Tar elementet navLinks
 let navLinks = document.getElementById("navLinks");
 
+//Funktionen showenu gör att menyn visas när man trycker på hamburgareikonen
 function showMenu(){
     navLinks.style.right = "0";
 }
 
+//Gömmer menyn
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
--- Tennis/Javascript/bokning.js
+++ Tennis/Javascript/bokning.js
@@ -1,53 +1,70 @@
 //Validering
 
+//lägger dagens datum i variablen currentDate
 let currentDate = new Date();
 
+//Tar ut året från currentDate och lägger den i variablen year
 let year = currentDate.getFullYear();
 
+//Tar ut månad
 let month = currentDate.getMonth() + 1;
 
+//Tar ut dag
 let day = currentDate.getDate();
 
+//Tar ut timmar
 let hour = currentDate.getHours();
 
+//Tar ut minuter
 let minutes = currentDate.getMinutes ();
 
+//Lägger till en "0" i månad ifall månaden är tidigare än oktober=10
 if (month < 10) {
     month = "0" + month;
 }
 
+//Gör samma sak fast med dagar
 if (day < 10) {
     day = "0" + day;
 }
 
+//lägger till dagens datum i dagensDatum i rätt format
 let dagensDatum = year + "-" + month + "-" + day; 
 
+//lägger till "0" i timmar ifall klockan är innan 10
 if (hour < 10) {
     hour = "0" + hour;
 }
 
+//Gör samma sak fast med minuter
 if (minutes < 10) {
     minutes = "0" + minutes;
 }
 
+//Lägger till tiden i rätt format
 let tidenNu = hour + ":" + minutes;
 
+//Tar ut första elementet
 let datumElem = document.querySelector("#datum");
 let tidElem = document.querySelector("#tid");
 
+//Sätter minimum på dagens datum och nuvarande tid
 datumElem.setAttribute ("min", dagensDatum);
 tidElem.setAttribute ("min", tidenNu);
 
+//Validering
 function validering() {
 
     let currentDate = new Date();
 
+    //Tar ut värdet från inputen och lägger i variabler
     const förNamn = document.getElementById("fname").value;
     const efterNamn = document.getElementById("ename").value;
     const nummer = document.getElementById("nummer").value;
     const datum = document.getElementById("datum").value;
     const tid = document.getElementById("tid").value;
 
+    //Validering för alla fällt ifall det lämnas tomt
     if (förNamn.length < 1) {
         window.alert("Ogiltigt förnamn");
         return false;
@@ -78,6 +95,7 @@ function validering() {
 
 }
 
+//Gömmer bokningsrutan
 let hideResult = document.getElementById("anvandarData");
 hideResult.style.display = "none";
 
@@ -87,12 +105,14 @@ hideText.style.display = "none";
 //LocalStorage
 function saveBokForm(event) {
 
+    //om valideringen går igenom så fortsätts saveBokForm
       if (!validering()) {
         return;
       }
 
       let bokningarArray = new Array();
 
+      //Lägger in all input i formData
       const formData = {
         Bana: document.getElementById("bana").value,
         FörNamn: document.getElementById("fname").value,
@@ -108,20 +128,18 @@ function saveBokForm(event) {
         bokning: "TennisBana",
       }
 
+
       if (localStorage.getItem("bokning")) {
         allaAnvandareArray = JSON.parse(localStorage.getItem("bokning"));
       }
 
-
-      if (document.getElementById("bastu").checked === "true") {
-        "bastu".checked = "ja";
-      }
-
+      //Lägger in formData i array
       bokningarArray.push(formData);
       
+      //lägger in arrayen i local storage i string form
       localStorage.setItem("bokning", JSON.stringify(bokningarArray));
       
-
+      //Loggar arrayen i consolen
       console.log(JSON.parse(localStorage.getItem("bokning")));
 
       const anvandarData = document.getElementById("anvandarData");
@@ -130,6 +148,7 @@ function saveBokForm(event) {
 
       tableArray = JSON.parse(localStorage.getItem("bokning"));
 
+      //Skriver över arrayen i variablen senasteBokningen
       const senasteBokningen = tableArray.pop();
 
     const värden = [
@@ -160,6 +179,7 @@ function saveBokForm(event) {
         "Hyra av racket:",
     ];
 
+    //skapar en table för utskriften av bokningen
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
 
@@ -189,6 +209,7 @@ function saveBokForm(event) {
 
     anvandarData.appendChild(tbl);
 
+    //gömmer table innan man trycker på boka knappen
     if (hideResult.style.display === "none") {
         hideResult.style.display = "flex";
     }
